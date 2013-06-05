ac767e1da054f7324083ef7d248f389c6f6d4f94
Jan Machacek <janm@cakesolutions.net>
2013-06-05 12:27:40 +0100

    Encoder comments

--- ios/Streamer/CV/ImageEncoder.h
+++ ios/Streamer/CV/ImageEncoder.h
@@ -1,8 +1,40 @@
 #import <Foundation/Foundation.h>
 #import <CoreMedia/CoreMedia.h>
 
+/**
+ * Image format representation
+ */
+enum ImageFormat {
+	PNG,
+	JPEG
+};
+
 @interface ImageEncoder : NSObject
 
+  /**
+   * Encodes the ``CMSampleBufferRef`` by turning it into a ``CGImageRef`` and applying the
+   * ``preflight`` function to it. If it returns ``true``, it will be turned into
+   * the desired image format, resized, and the ``success`` function applied to the
+   * bytes that make up the image.
+   */
 - (void)encode:(CMSampleBufferRef)frame withPreflight:(bool (^)(CGImageRef))preflight andSuccess:(void (^)(NSData*))success;
+
+// TODO: include properties
+
+/**
+ * if != nil, the compression quality for the JPEG compression. Ignored when
+ * ``imageFormat`` is ``PNG``
+ */
+@property NSNumber *jpegImageQuality;
+/**
+ * The image format: either PNG or JPEG
+ */
+@property ImageFormat imageFormat;
+/**
+ * if != nil, the desired image size. The code will keep the aspect ratio of the image.
+ * If you have 1020 * 720 image and ask it to be resized to 1020 * 800, nothing will
+ * happen.
+ */
+@property CGSize* resizeTo;
 
 @end
